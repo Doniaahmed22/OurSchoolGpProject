1472689acedf512a9e008c68d18f764306948a15
Hassan-Ali-Mohamed <119622080+Hassan-Ali-Mohamed@users.noreply.github.com>
2024-04-29 17:09:29 +0300

    Create ClassController.cs
    Add Class Controller

---
+++ School.API/Controllers/ClassController.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School.Data.Context;
+using School.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace School.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private readonly SchoolDbContext _context;
+
+        public ClassController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetClasses")]
+        public async Task<IActionResult> GetClasses()
+        {
+            var classes = await _context.Classes.ToListAsync();
+            return Ok(classes);
+        }
+
+        [HttpGet]
+        [Route("GetClassById/{id}")]
+        public async Task<IActionResult> GetClassById(int id)
+        {
+            var classItem = await _context.Classes.FindAsync(id);
+            if (classItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(classItem);
+        }
+
+        [HttpPost]
+        [Route("AddClass")]
+        public async Task<IActionResult> AddClass(Class classItem)
+        {
+            _context.Classes.Add(classItem);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("UpdateClass")]
+        public async Task<IActionResult> UpdateClass(Class classItem)
+        {
+            if (classItem == null || classItem.Id == 0)
+            {
+                return BadRequest();
+            }
+
+            var existingClass = await _context.Classes.FindAsync(classItem.Id);
+            if (existingClass == null)
+            {
+                return NotFound();
+            }
+
+            existingClass.Name = classItem.Name;
+            existingClass.NumOfStudent = classItem.NumOfStudent;
+            existingClass.DepartmentId = classItem.DepartmentId;
+            existingClass.LevelId = classItem.LevelId;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteClass/{id}")]
+        public async Task<IActionResult> DeleteClass(int id)
+        {
+            var classItem = await _context.Classes.FindAsync(id);
+            if (classItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Classes.Remove(classItem);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
