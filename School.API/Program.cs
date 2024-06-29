d112a85079eec55bf831764c83c9919ac338a5b0
Doniaahmed22 <119499612+Doniaahmed22@users.noreply.github.com>
2024-06-29 23:30:00 +0300

    CombineDataBase

--- School.API/Program.cs
+++ School.API/Program.cs
@@ -18,54 +18,38 @@ namespace School.API
 {
     public class Program
     {
+
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
-            //builder.Services.AddControllers();
             builder.Services.AddControllers()
-            .AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-            });
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+                });
 
             builder.Services.AddDbContext<SchoolDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            builder.Services.AddDbContext<SchoolIdentityDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
-            });
-
-
-            builder.Services.AddControllersWithViews();
-
             builder.Services.AddIdentity<AppUser, IdentityRole>()
-                .AddEntityFrameworkStores<SchoolIdentityDbContext>()
+                .AddEntityFrameworkStores<SchoolDbContext>()
                 .AddDefaultTokenProviders();
 
             builder.Configuration.AddEnvironmentVariables();
-
             builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
             builder.Services.AddSingleton<EmailService>();
             builder.Services.AddLogging();
-            //builder.Services.AddSingleton<EmailService>();
-
             builder.Services.AddSchoolServices();
             builder.Services.AddIdentityServices(builder.Configuration);
 
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            
             builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerDocumentation();
             builder.Services.AddSwaggerGen();
-
 
             builder.Services.AddCors(corsoption =>
             {
@@ -74,8 +58,8 @@ namespace School.API
                     policybuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
                 });
             });
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             builder.Services.AddSingleton<TokenBlacklistService>();
 
             builder.Services.AddAuthorization(options =>
@@ -113,7 +97,6 @@ namespace School.API
                         }
                     };
 
-
                     options.Events = new JwtBearerEvents
                     {
                         OnTokenValidated = context =>
@@ -128,8 +111,6 @@ namespace School.API
                         }
                     };
                 });
-            //builder.Services.AddScoped<UserRoleFilter>(); // Register the custom action filter
-
 
             var app = builder.Build();
 
@@ -144,21 +125,8 @@ namespace School.API
 
             app.UseCors("MyPolicy");
 
-            /*
-            //builder.Services.AddAuthorization(options =>
-            //{
-            //    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-            //    options.AddPolicy("RequireParentRole", policy => policy.RequireRole("Parent"));
-            //    options.AddPolicy("RequireStudentRole", policy => policy.RequireRole("Student"));
-            //    options.AddPolicy("RequireTeacherRole", policy => policy.RequireRole("Teacher"));
-            //});
-            */
-
-
-            app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
@@ -175,3 +143,5 @@ namespace School.API
         }
     }
 }
+
+    
